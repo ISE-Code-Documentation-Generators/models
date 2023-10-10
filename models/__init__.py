a19b2e511aaa4fd4d4501bbecba4b1011486a639
Ashkan Khademian <ashkan.khd.q@gmail.com>
2023-10-10 23:12:49 +0330

    fix: fix importing issues

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
-from a_conv_gnn import AConvGNN
-from attention_seq2seq import AttentionSeq2Seq
-from seq2seq import Seq2Seq
+from .a_conv_gnn import AConvGNN
+from .attention_seq2seq import AttentionSeq2Seq
+from .seq2seq import Seq2Seq
