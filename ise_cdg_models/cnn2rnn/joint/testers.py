59a344fed60fae991401031ba8cc3fb939ccfc55
Ashkan Khademian <ashkan.khd.q@gmail.com>
2023-11-09 13:32:42 +0330

    fix: fix tester printer

--- ise_cdg_models/cnn2rnn/joint/testers.py
+++ ise_cdg_models/cnn2rnn/joint/testers.py
@@ -75,6 +75,6 @@ class CNN2RNNTesterOnDataset:
         for metric_name, metric in metrics_with_name.items():
             self.printer(f'x--- {metric_name} ---x')
             metric.set_references(mds)
-            self.printer(metric(candidates))
+            self.printer(str(metric(candidates)))
         self.printer('')
         return candidates, mds
